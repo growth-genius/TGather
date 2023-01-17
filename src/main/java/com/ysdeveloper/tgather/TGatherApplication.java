3b4585c4fa0cd87abf53c641c8187fb25f756de1
leeseunggu <leesg107@naver.com>
2023-01-17 19:36:32 +0900

    initial commit

---
+++ src/main/java/com/ysdeveloper/tgather/TGatherApplication.java
@@ -0,0 +1,13 @@
+package com.ysdeveloper.tgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGatherApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run( TGatherApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/ysdeveloper/tgather/TGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysdeveloper.tgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGatherApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
