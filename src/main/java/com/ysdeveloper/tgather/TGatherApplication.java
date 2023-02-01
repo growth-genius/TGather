9613a2576407db63b54fcae4a2d9b3736172df01
yeji.jo <choyeji1591@gmail.com>
2023-02-01 23:39:18 +0900

    fix : 중복 어노테이션 제거

--- src/main/java/com/ysdeveloper/tgather/TGatherApplication.java
+++ src/main/java/com/ysdeveloper/tgather/TGatherApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EncryptablePropertySource( name = "EncryptedProperties", value = "classpath:encrypted.yml" )
