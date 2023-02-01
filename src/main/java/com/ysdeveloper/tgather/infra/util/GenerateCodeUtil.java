52e75d6e95ef9f1c565f4b8fd7660f57f8bcbd02
yeji.jo <choyeji1591@gmail.com>
2023-02-01 23:37:29 +0900

    feat : 랜덤 otpCode 생성

--- src/main/java/com/ysdeveloper/tgather/infra/util/GenerateCodeUtil.java
+++ src/main/java/com/ysdeveloper/tgather/infra/util/GenerateCodeUtil.java
@@ -1,13 +1,12 @@
 package com.ysdeveloper.tgather.infra.util;
 
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 
 @NoArgsConstructor
 public class GenerateCodeUtil {
 
     public static String generateCode () {
-        String code = "";
-
-        return code;
+        return RandomStringUtils.randomAlphanumeric( 12 );
     }
 }
