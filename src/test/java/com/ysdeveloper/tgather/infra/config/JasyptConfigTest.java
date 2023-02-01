ff2c98dfb730945405b993289c8dcc7090774543
leeseunggu <leesg107@naver.com>
2023-02-01 20:37:38 +0900

    change column user_name to username

--- src/test/java/com/ysdeveloper/tgather/infra/config/JasyptConfigTest.java
+++ src/test/java/com/ysdeveloper/tgather/infra/config/JasyptConfigTest.java
@@ -1,13 +1,15 @@
 package com.ysdeveloper.tgather.infra.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class JasyptConfigTest {
 
@@ -15,24 +17,27 @@ class JasyptConfigTest {
     private StringEncryptor jasyptStringEncryptor;
 
     @Test
-    void _0_testInit() {
+    void _0_testInit () {
         assertThat( jasyptStringEncryptor ).isNotNull();
     }
 
     @Test
-    void _1_encrypt_decrypt() {
+    void _1_encrypt_decrypt () {
 
         String orgText = "dPwldidnfltjdrhdgkwk";
 
         String encText = jasyptStringEncryptor.encrypt( orgText );
 
-        System.out.println("encText:: " + encText);
+        System.out.println( "encText:: " + encText );
 
         String decrypt = jasyptStringEncryptor.decrypt( encText );
 
-        System.out.println("decrypt:: {}"+ decrypt);
+        System.out.println( "decrypt:: {}" + decrypt );
 
         assertEquals( orgText, decrypt );
+
+        log.error( jasyptStringEncryptor.encrypt( "AKIAXSCQ3NCOPIDPY2JG" ) );
+        log.error( jasyptStringEncryptor.encrypt( "dnk4uTi/RxxZbIfxMm62fV0+aWdJ7E8BW7wQjQwp" ) );
 
     }
 }
