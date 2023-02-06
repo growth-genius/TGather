72ff3f2ffeb9722edf8af8261abc5a46b475b0eb
yeji.jo <choyeji1591@gmail.com>
2023-02-06 11:41:18 +0900

    fix : nickname 확인

--- src/main/java/com/ysdeveloper/tgather/modules/account/repository/AccountRepository.java
+++ src/main/java/com/ysdeveloper/tgather/modules/account/repository/AccountRepository.java
@@ -3,8 +3,14 @@ package com.ysdeveloper.tgather.modules.account.repository;
 import com.ysdeveloper.tgather.modules.account.entity.Account;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional( readOnly = true )
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByUsername ( String username );
+
+    Optional<Account> findByNickname ( String nickname );
 }
