8a0c41d2a6c4fd0673a379e73a0d6c881d169c27
yeji.jo <choyeji1591@gmail.com>
2023-04-18 22:17:42 +0900

    fix : Profile local 삭제

--- src/main/java/com/ysdeveloper/tgather/infra/mail/HtmlEmailService.java
+++ src/main/java/com/ysdeveloper/tgather/infra/mail/HtmlEmailService.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Profile( { "dev", "prod", "local" } )
+@Profile( { "dev", "prod" } )
 @Component
 @RequiredArgsConstructor
 public class HtmlEmailService implements EmailService {
