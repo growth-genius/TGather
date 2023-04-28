e536134b3e99f4ac714d58685a12795e74f0a421
leeseunggu <leesg6107@gmail.com>
2023-04-28 20:57:32 +0900

    fix: downgrade springboot 3.0.1 -> 3.0.0

--- src/main/java/com/ysdeveloper/tgather/modules/account/AccountController.java
+++ src/main/java/com/ysdeveloper/tgather/modules/account/AccountController.java
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,28 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping( "/api/account" )
+@RequestMapping("/api/account")
 public class AccountController {
 
     private final AccountService accountService;
     private final AuthenticationManager authenticationManager;
 
-    @PostMapping( "/sign-up" )
-    public ResponseEntity<AccountDto> addUser ( @RequestBody @Valid AccountSaveForm accountSaveForm ) {
-        return ResponseEntity.ok( accountService.saveAccount( accountSaveForm ) );
+    @PostMapping("/sign-up")
+    public ResponseEntity<AccountDto> addUser(@RequestBody @Valid AccountSaveForm accountSaveForm) {
+        return ResponseEntity.ok(accountService.saveAccount(accountSaveForm));
     }
 
-    @PostMapping( "/check-email" )
-    public ResponseEntity<AccountDto> authCode ( @RequestBody @Valid AuthCodeForm authCodeForm ) {
-        return ResponseEntity.ok( accountService.validAuthCode( authCodeForm ) );
+    @PostMapping("/check-email")
+    public ResponseEntity<AccountDto> authCode(@RequestBody @Valid AuthCodeForm authCodeForm) {
+        return ResponseEntity.ok(accountService.validAuthCode(authCodeForm));
     }
 
-    @PostMapping( "/login" )
-    public ResponseEntity<AccountDto> login ( @RequestBody @Valid AccountSaveForm accountSaveForm ) {
-        authenticationManager.authenticate(
-                new JwtAuthenticationToken( accountSaveForm.getEmail(), new CredentialInfo( accountSaveForm.getPassword() ) )
-        );
-        return ResponseEntity.ok( accountService.findByEmail( accountSaveForm.getEmail(), LoginType.TGAHTER) );
+    @PostMapping("/login")
+    public ResponseEntity<AccountDto> login(@RequestBody @Valid AccountSaveForm accountSaveForm) {
+        authenticationManager.authenticate(new JwtAuthenticationToken(accountSaveForm.getEmail(), new CredentialInfo(accountSaveForm.getPassword())));
+        return ResponseEntity.ok(accountService.findByEmail(accountSaveForm.getEmail(), LoginType.TGAHTER));
     }
 
 }
