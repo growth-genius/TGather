b42d9a481226b6d0da1e28086737943d59a704e9
yeji.jo <choyeji1591@gmail.com>
2023-05-10 23:04:59 +0900

    feat : travelGroup 참여자수 제한 필드 추가

--- src/main/java/com/ysdeveloper/tgather/modules/travelgroup/entity/TravelGroup.java
+++ src/main/java/com/ysdeveloper/tgather/modules/travelgroup/entity/TravelGroup.java
@@ -26,21 +26,23 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-@NamedEntityGraph( name = "TravelGroup.withTravelThemes", attributeNodes = { @NamedAttributeNode( "travelThemes" ) } )
+@NamedEntityGraph(name = "TravelGroup.withTravelThemes", attributeNodes = {@NamedAttributeNode("travelThemes")})
 public class TravelGroup extends UpdatedEntity {
 
+    private static final int MAX_PARTICIPANT_COUNT = 999;
+
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
-    @Column( name = "travel_group_id" )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "travel_group_id")
     private Long id;
 
     /* 여행 만남 이름 */
     private String groupName;
 
     /* 여행 테마 (복수 선택 가능 )*/
-    @ElementCollection( fetch = FetchType.LAZY )
-    @Enumerated( EnumType.STRING )
-    @CollectionTable( name = "travel_themes", joinColumns = @JoinColumn( name = "travel_group_id" ) )
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "travel_themes", joinColumns = @JoinColumn(name = "travel_group_id"))
     private Set<TravelTheme> travelThemes;
 
     /** 만남 참여자 수 */
@@ -49,28 +51,33 @@ public class TravelGroup extends UpdatedEntity {
     /** 공개 여부 */
     private boolean open = true;
 
+    /** 참여자 수 제한 **/
+    private long limitParticipantCount;
+
     /** 여행 시작일 */
     private String startDate;
 
     /* 권한 그룹 소속 유저들 */
-    @OneToMany( mappedBy = "travelGroup", fetch = FetchType.LAZY )
-    private List<TravelGroupMember> travelGroupMemberList = new ArrayList<>();
+    @OneToMany(mappedBy = "travelGroup", fetch = FetchType.LAZY)
+    private final List<TravelGroupMember> travelGroupMemberList = new ArrayList<>();
 
-    public TravelGroup ( TravelGroupForm travelGroupForm ) {
+    public TravelGroup(TravelGroupForm travelGroupForm) {
         this.groupName = travelGroupForm.getGroupName();
         this.travelThemes = travelGroupForm.getTravelThemes();
         this.startDate = travelGroupForm.getStartDate();
+        this.open = travelGroupForm.isOpen();
+        this.limitParticipantCount = travelGroupForm.isLimitedParticipant() ? travelGroupForm.getLimitParticipantCount() : MAX_PARTICIPANT_COUNT;
     }
 
-    public static TravelGroup from ( TravelGroupForm travelGroupForm ) {
-        return new TravelGroup( travelGroupForm );
+    public static TravelGroup from(TravelGroupForm travelGroupForm) {
+        return new TravelGroup(travelGroupForm);
     }
 
-    public void plusParticipant () {
+    public void plusParticipant() {
         this.participantCount++;
     }
 
-    public void minusParticipant () {
+    public void minusParticipant() {
         this.participantCount--;
     }
 
